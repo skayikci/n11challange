3aa76bf628557af1c2031f5071ac4fd164688631
skayikci <serhat.kayikci@gmail.com>
2016-10-11 16:21:19 +0300

    delete node

--- node_modules/ng-table/src/browser/ngTableFilterConfig.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * The angular provider used to configure the behaviour of the `ngTableFilterConfig` service.
- *
- * Implements the {@link IFilterConfigProvider IFilterConfigProvider} interface
- */
-export declare function ngTableFilterConfigProvider(): void;
--- node_modules/ng-table/src/core/index.d.ts
+++
@@ -1,5 +0,0 @@
-/// <reference types="angular" />
-import * as angular from 'angular';
-declare var _default: angular.IModule;
-export default _default;
-export * from './public-interfaces';
